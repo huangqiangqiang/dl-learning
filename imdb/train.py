7e81a96ac8eba826c93743570db6addc0e4a7a2b
hqqsk8 <285086598@163.com>
2021-06-08 21:05:23 +0800

    update

--- imdb/train.py
+++ imdb/train.py
@@ -1,20 +1,49 @@
+import numpy as np
 import tensorflow as tf;
 from tensorflow.keras.datasets import imdb;
+from tensorflow.keras import models
+from tensorflow.keras import layers
+
+def createModel():
+  model = models.Sequential()
+  model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+  model.add(layers.Dense(16, activation='relu'))
+  model.add(layers.Dense(1, activation='sigmoid'))
+  return model
 
 def train():
-    # 仅仅返回数据中最常出现的前 10000 个单词
     (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
     print(train_data.shape)
-    print( train_data[0])
-    reverse_word_index = dict()
-    word_index = imdb.get_word_index()
-    for (key, value) in word_index.items():
-        reverse_word_index[value] = key
-    print(reverse_word_index)
+    x_train = vectorize_sequences(train_data)
+    x_test = vectorize_sequences(test_data)
+    y_train = np.asarray(train_labels).astype('float32')
+    y_test = np.asarray(test_labels).astype('float32')
+    # print(decoded_review)
 
+    model = createModel()
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
+    # reverse_word_index = dict()
+    # word_index = imdb.get_word_index()
+    # for (key, value) in word_index.items():
+    #     reverse_word_index[value] = key
+
+    # print(reverse_word_index)
+    # print('---------------------------------------------')
+    # print(train_data[0])
+    # print('---------------------------------------------')
+    # decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+
+def vectorize_sequences(sequences, dimension=10000):
+  results = np.zeros((len(sequences), dimension))
+  for i, sequence in enumerate(sequences):
+    results[i, sequence] = 1.
+  return results
+    
+
+# docker run -it --rm --name tensorflow-cpu -v $(pwd):/app tensorflow/tensorflow:2.4.1 bash
 if __name__ == '__main__':
   print(tf.__version__)
 
-  # 训练
   train()
+
