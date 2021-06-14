a4ed79e08a246f8e6b0e5b36e7f9da19636ee2ad
hqqsk8 <285086598@163.com>
2021-06-14 19:43:49 +0800

    update

--- imdb/train.py
+++ imdb/train.py
@@ -3,14 +3,15 @@ import tensorflow as tf;
 from tensorflow.keras.datasets import imdb;
 from tensorflow.keras import models
 from tensorflow.keras import layers
+import matplotlib.pyplot as plt
 
 
 def createModel():
-  model = models.Sequential()
-  model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-  model.add(layers.Dense(16, activation='relu'))
-  model.add(layers.Dense(1, activation='sigmoid'))
-  return model
+    model = models.Sequential()
+    model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+    model.add(layers.Dense(16, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+    return model
 
 def train():
     (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
@@ -19,11 +20,41 @@ def train():
     x_test = vectorize_sequences(test_data)
     y_train = np.asarray(train_labels).astype('float32')
     y_test = np.asarray(test_labels).astype('float32')
+
     # print(decoded_review)
 
     model = createModel()
     model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
+    x_val = x_train[:10000]
+    y_val = y_train[:10000]
+    partial_x_train = x_train[10000:]
+    partial_y_train = y_train[10000:]
+    history = model.fit(partial_x_train, partial_y_train, batch_size=512, epochs=20, validation_data=(x_val, y_val))
+
+    print(history.history)
+
+    results  = model.evaluate(x_test, y_test)
+
+    print(results)
+
+    res = model.predict(x_test)
+
+    print(res)
+    
+    # show graph
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, loss, 'bo', label='training loss')
+    plt.plot(epochs, val_loss, 'b', label='validation loss')
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.show()
+    
     # reverse_word_index = dict()
     # word_index = imdb.get_word_index()
     # for (key, value) in word_index.items():
