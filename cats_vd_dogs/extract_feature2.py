6c4f71fbd6cae2b36d9717ca06249bbef4c1fafe
hqqsk8 <285086598@163.com>
2021-07-03 17:26:38 +0800

    update

--- cats_vd_dogs/extract_feature2.py
+++ cats_vd_dogs/extract_feature2.py
@@ -1,8 +1,8 @@
 from scipy.ndimage.measurements import label
-from cats_vd_dogs.train import createModel
 import os
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import optimizers
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Activation, MaxPooling2D, Flatten, Dropout
@@ -16,51 +16,46 @@ import matplotlib.pyplot as plt
 这里使用的是提取 VGG16 模型的卷积层，去掉后面的全连接层。
 因为 VGG16 模型能识别上千种分类，最后的输出层也是有上千个神经元，虽然包含了不同种类的猫和狗，但是我们想要的只是能识别猫狗就行了。
 所以我们自定义了全连接层和输出层。
-
-这个例子中，我们提取了 VGG16 的卷积层，也就是 conv_base，然后我们把训练集传入 conv_base 预测，预测的输出先展开，再做为我们自定义模型的输入。
 """
 
-conv_base = VGG16(include_top=False, weights='imagenet', input_shape=(150,150,3))
+conv_base = VGG16(include_top=False, weights='imagenet', input_shape=(150, 150, 3))
 
 base_dir = '../../dataset/cats_and_dogs_small'
 train_dir = base_dir + '/train'
 val_dir = base_dir + '/validation'
 test_dir = base_dir + '/test'
 
-datagen = image.ImageDataGenerator(rescale=1./255)
-batch_size = 20
-
-def extract_features(directory, sample_count):
-  features = np.zeros(shape=(sample_count, 4, 4, 512))
-  labels = np.zeros(shape=(sample_count))
-  generater = datagen.flow_from_directory(directory=directory, target_size=(150, 150), batch_size=batch_size, class_mode='binary')
-  i = 0
-  for inputs_batch, labels_batch in generater:
-    features_batch = conv_base.predict(inputs_batch)
-    features[i * batch_size:(i + 1) * batch_size] = features_batch
-    labels[i * labels_batch:(i + 1) * labels_batch] = labels_batch
-    i += 1
-    # if i * batch_size >= sample_count:
-    #   break
-  return features, labels
-
 def train():
-  train_features, train_labels = extract_features(train_dir, 2000)
-  val_features, val_labels = extract_features(val_dir, 2000)
-  # test_features, test_labels = extract_features(test_dir, 2000)
+  # 使用数据增强
+  train_datagen = image.ImageDataGenerator(
+    rescale=1./255, 
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest' 
+  )
+  val_datagen = image.ImageDataGenerator(rescale=1./255)
 
-  # 展开
-  train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
-  val_features = np.reshape(val_features, (1000, 4 * 4 * 512))
+  train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
+  val_generator = val_datagen.flow_from_directory(val_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
+
+  # 冻结 conv_base 的权重，训练时不会改变，需要在模型编译前设置
+  conv_base.trainable = False
 
   model = Sequential()
+  model.add(conv_base)
+  model.add(Flatten())
   model.add(Dense(256, activation='relu', input_dim=4 * 4 * 512))
   model.add(Dropout(0.5))
   model.add(Dense(1, activation='sigmoid'))
+  model.summary()
+  model.compile(optimizer=optimizers.RMSprop(learning_rate=2e-5), loss='binary_crossentropy', metrics=['acc'])
 
-  model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-  history = model.fit(train_features, train_labels, epochs=30, batch_size=20, validation_data=(val_features, val_labels))
-  
+  history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=30, validation_data=val_generator, validation_steps=50)
+
   # 画图
   acc = history.history['acc']
   loss = history.history['loss']
@@ -81,6 +76,7 @@ def train():
   plt.legend()
 
   plt.show()
+
 
 if __name__ == '__main__':
   print(tf.__version__)
